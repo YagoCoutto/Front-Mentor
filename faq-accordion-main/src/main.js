d56bea32180d4a64b6d99fb45c0be5f0c029fed4
Yago Coutto <62577739+YagoCoutto@users.noreply.github.com>
2024-04-07 21:30:57 -0300

    update

--- faq-accordion-main/src/main.js
+++ faq-accordion-main/src/main.js
@@ -1,22 +1,24 @@
 const questionBox = document.querySelectorAll('.question-box');
 const responseBox = document.querySelectorAll('.response-box');
-
+const questionsImg = document.querySelectorAll('.icon');
 
 function showAnswer(question) {
     let isActive = false;
     question.forEach((element, index) => {
         element.addEventListener("click", () => {
-            if (!isActive) {
-                isActive = true;
-                ver(index)
-            } else {
-                isActive = false;
-                ex(index)
-            }
+            isActive === false ? showAnswer(index) : hideAnswer(index)
         })
     });
 
-    const ver = (index) => responseBox[index].style.display = 'flex'; 
-    const ex = (index) => responseBox[index].style.display = 'none';
+    const showAnswer = (index) => {
+        const iconMinus = questionBox[index].children[1].src = "assets/images/icon-minus.svg";
+        const displayFlex = responseBox[index].style.display = 'flex';
+        isActive = true;
+    }
+    const hideAnswer = (index) => {
+        const iconPlus = questionBox[index].children[1].src = "assets/images/icon-plus.svg";
+        const displayNone = responseBox[index].style.display = 'none';
+        isActive = false;
+    }
 }
 showAnswer(questionBox)
